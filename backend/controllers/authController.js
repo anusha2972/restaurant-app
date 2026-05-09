c4838a814d76d75623bfd28c29ebdbe2719277c6
anusha2972 <98248212+anusha2972@users.noreply.github.com>
2026-05-09 17:58:32 -0400

    Added login function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// Genereate JWT token
+// Generate JWT token
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, 
         {
@@ -60,3 +60,51 @@ const registerUser = async(request, response) => {
         response.status(500).json({message: error.message});
     }
 };
+
+// @desc Login a user
+// @ruute POST /api/auth/login
+// @access Public
+const loginUser = async(request, response) => {
+    try
+    {
+        // Destructure the request body
+        const {email, password} = request.body;
+        
+        //Check if all the fields are provided
+        if(!email || !password)
+        {
+            return response.status(400).json({message: 'Please provide all the fields'});
+        }
+
+        // Check if the user exists
+        const user = await User.findOne(email);
+        if(!user)
+        {
+            return response.status(401).json({message: 'Invalid email or password'});
+        }
+
+        // Check if the entered password matches the hashed password in the database
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch)
+        {
+            return response.status(401).json({message: 'Invalid email or password'});
+        }
+
+        // Send the user data and token as respomse
+        response.status(200).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id),
+        });
+    }
+    catch(error)
+    {
+        response.status(500).json({message:error.message});
+    }
+        
+}
+
+// Export the controller functions
+module.exports = {registerUser, loginUser};
